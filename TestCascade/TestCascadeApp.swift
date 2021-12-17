f5c74f2d97d48c1bbef044317e9c0c8dc3ea21e2
Isaak <isaak.hanimann@gmail.com>
2021-12-17 10:03:12 +0100

    Initial Commit

---
+++ TestCascade/TestCascadeApp.swift
@@ -0,0 +1,20 @@
+//
+//  TestCascadeApp.swift
+//  TestCascade
+//
+//  Created by Isaak Hanimann on 17.12.21.
+//
+
+import SwiftUI
+
+@main
+struct TestCascadeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
